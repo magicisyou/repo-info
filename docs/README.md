2e4e1b13eadd5654c6a39b18fc2488f52dfa9627
youaremagic <145214127+youaremagic@users.noreply.github.com>
2023-12-04 21:02:16 +0530

    added more details, added progress bar, v0.3.0

--- docs/README.md
+++ docs/README.md
@@ -1,21 +1,19 @@
-# Repo lang stat
+# Repo Info
 
-A cli tool to print the statistics of programming languages used in a github repository
+A cli tool to get the details of a github repository
 
 ## About
 
-This is a cli tool which fetches the programming languages used in a repository and print the percentage and number of characters in the terminal. Since no token is used there would be limits in using github api (about 60 request per hour).
+This is a cli tool which fetches the details of a github repository. It also shows the programming languages used in a repository and print the percentage and a bargraph in the terminal. Since no token is used there would be limits in using github api (about 60 request per hour).
 
-## Screenshot
-
-![Example](/docs/screenshot.webp)
+![Screenshot](/docs/screenshot.webp)
 
 ## Build
 
 Clone the directory
 
 ```
-git clone https://github.com/youaremagic/repo-lang-stat
+git clone https://github.com/youaremagic/repo-info
 ```
 
 Change directory
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
 use clap::Parser;
 use colored::Colorize;
+use indicatif::ProgressBar;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, USER_AGENT};
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
 use term_size::dimensions;
+
+#[derive(serde::Deserialize)]
+struct RepositoryInfo {
+    name: String,
+    full_name: String,
+    description: String,
+    language: Option<String>,
+    stargazers_count: u32,
+    watchers_count: u32,
+    size: u32,
+    forks: u32,
+    open_issues: u32,
+    default_branch: String,
+    updated_at: String,
+    html_url: String,
+    #[serde(flatten)]
+    _ignore: serde_json::Value,
+}
+
+struct Repository {
+    info: RepositoryInfo,
+    languages: HashMap<String, u64>,
+}
+
+impl Repository {
+    fn from(info: RepositoryInfo, languages: HashMap<String, u64>) -> Self {
+        Self { info, languages }
+    }
+}
 
 /// Get Info about languages used in a github repository
 #[derive(Parser)]
@@ -13,8 +43,12 @@ struct Details {
 }
 
 impl Details {
-    fn fetch(&self) -> Result<HashMap<String, u64>, Box<dyn std::error::Error>> {
-        let url = format!("https://api.github.com/repos/{}/languages", self.repository);
+    fn fetch_repository_details(&self) -> Result<Repository, Box<dyn std::error::Error>> {
+        let progress_bar = ProgressBar::new(2);
+        progress_bar.inc(0);
+
+        let info_url = format!("https://api.github.com/repos/{}", self.repository);
+        let lang_url = format!("https://api.github.com/repos/{}/languages", self.repository);
 
         let mut headers = HeaderMap::new();
         headers.insert(
@@ -24,20 +58,62 @@ impl Details {
         headers.insert(USER_AGENT, HeaderValue::from_static("repo_lang_stat"));
 
         let client = reqwest::blocking::Client::new();
-        let response = client
-            .get(url)
+
+        let repository_info = client
+            .get(info_url)
+            .headers(headers.clone())
+            .send()?
+            .json::<RepositoryInfo>()?;
+
+        progress_bar.inc(1);
+
+        let repository_languages = client
+            .get(lang_url)
             .headers(headers)
             .send()?
             .json::<HashMap<String, u64>>()?;
 
-        Ok(response)
+        progress_bar.inc(1);
+        progress_bar.finish_and_clear();
+
+        Ok(Repository::from(repository_info, repository_languages))
+    }
+}
+
+impl fmt::Display for RepositoryInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let term_width = match dimensions() {
+            Some((width, _)) => width,
+            None => 65,
+        };
+        let language = match &self.language {
+            Some(lang) => lang.into(),
+            None => "".to_string(),
+        };
+        writeln!(f, "{:term_width$}", self.name.red().on_white())?;
+        writeln!(f, "Full Name: {}", self.full_name.red())?;
+        writeln!(f, "Description: {}", self.description.red())?;
+        writeln!(f, "Language: {}", language.red())?;
+        writeln!(f, "Default branch: {}", self.default_branch.red())?;
+        writeln!(f, "Stars: {}", self.stargazers_count.to_string().red())?;
+        writeln!(f, "Watchers: {}", self.watchers_count.to_string().red())?;
+        writeln!(f, "Size: {}", self.size.to_string().red())?;
+        writeln!(f, "Forks: {}", self.forks.to_string().red())?;
+        writeln!(f, "Open issues: {}", self.open_issues.to_string().red())?;
+        writeln!(f, "Last updated: {}", self.updated_at.red())?;
+        write!(f, "Link: {}", self.html_url.red())?;
+        Ok(())
     }
 }
 
 fn main() {
     let details = Details::parse();
-    match details.fetch() {
-        Ok(response) => print_stat(&response),
+
+    match details.fetch_repository_details() {
+        Ok(response) => {
+            println!("{}", &response.info);
+            print_stat(&response.languages);
+        }
         Err(e) => eprintln!("Sorry, Can't find what you are looking for.\nError : {e}"),
     }
 }
@@ -46,17 +122,17 @@ fn print_stat(data: &HashMap<String, u64>) {
     if data.is_empty() {
         println!("No languages found");
     } else {
+        println!("Languages:");
         let mut sum = 0;
         for (_, count) in data.iter() {
             sum += count;
         }
-        println!();
         for (lang, count) in data.iter() {
             let bar = get_bar(sum, *count);
             println!(
                 "{:15}  {:6}  {}",
-                lang.blue(),
-                percentage(sum, *count).green(),
+                lang,
+                percentage(sum, *count).red(),
                 bar.green(),
             );
         }
