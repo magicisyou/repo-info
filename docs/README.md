76e111812554e6a705813fd8b7206ccfa9f8225a
youaremagic <145214127+youaremagic@users.noreply.github.com>
2023-12-04 16:26:53 +0530

    added graph, used single arguement, v0.2.0

--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 # Repo lang stat
 
-A small cli tool to print the statistics of programming languages used in a github repository
+A cli tool to print the statistics of programming languages used in a github repository
 
 ## About
 
-This is a small tool which fetches the programming languages used in a repository and print the percentage and number of characters in the terminal. Since no token is used there would be limits in using github api (about 60 request per hour).
+This is a cli tool which fetches the programming languages used in a repository and print the percentage and number of characters in the terminal. Since no token is used there would be limits in using github api (about 60 request per hour).
 
 ## Screenshot
 
@@ -24,8 +24,8 @@ Change directory
 cd repo-lang-stat
 ```
 
-Run
+Build
 
 ```
-cargo run --release -- <OWNER> <REPO>
+cargo build --release
 ```
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,19 @@ use clap::Parser;
 use colored::Colorize;
 use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, USER_AGENT};
 use std::collections::HashMap;
+use term_size::dimensions;
 
 /// Get Info about languages used in a github repository
 #[derive(Parser)]
 #[command(version)]
 struct Details {
-    /// Owner of the repository
-    owner: String,
-    /// Name of the repository
-    repo: String,
+    /// Repository in format owner/repository
+    repository: String,
 }
 
 impl Details {
     fn fetch(&self) -> Result<HashMap<String, u64>, Box<dyn std::error::Error>> {
-        let url = format!(
-            "https://api.github.com/repos/{}/{}/languages",
-            self.owner, self.repo
-        );
-
-        println!("Fetching info!");
+        let url = format!("https://api.github.com/repos/{}/languages", self.repository);
 
         let mut headers = HeaderMap::new();
         headers.insert(
@@ -58,10 +52,12 @@ fn print_stat(data: &HashMap<String, u64>) {
         }
         println!();
         for (lang, count) in data.iter() {
+            let bar = get_bar(sum, *count);
             println!(
-                "{:12} : {:6} : {count} chars",
+                "{:15}  {:6}  {}",
                 lang.blue(),
-                percentage(sum, *count).green()
+                percentage(sum, *count).green(),
+                bar.green(),
             );
         }
     }
@@ -70,4 +66,19 @@ fn print_stat(data: &HashMap<String, u64>) {
 fn percentage(sum: u64, count: u64) -> String {
     let perc = (count as f64 / sum as f64) * 100.0;
     format!("{:.2}%", perc)
+}
+
+fn get_bar(sum: u64, count: u64) -> String {
+    let text_width = 30;
+    let fallback_width = 60;
+    let term_width = match dimensions() {
+        Some((width, _)) => width as u64 - text_width,
+        None => fallback_width - text_width,
+    };
+    let width = (count * term_width / sum) as usize;
+    let mut bar = String::from("");
+    for _i in 0..width {
+        bar.push('■');
+    }
+    bar
 }
